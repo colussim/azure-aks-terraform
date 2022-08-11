0aaa25bb13181c0a3d7976f5c7c45a50afa96610
Emmanuel COLUSSI <emmanuel.colussi@hpe.com>
2022-08-11 10:56:42 +0200

    Update aks-cluster.tf

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -88,8 +88,8 @@ resource "azurerm_network_security_rule" "edb-https" {
   destination_port_range      = "443"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.EDB_aks_rg.name}"
-  network_security_group_name = "${azurerm_network_security_group.network-sg01.name}"
+  resource_group_name         = azurerm_resource_group.EDB_aks_rg.name
+  network_security_group_name = azurerm_network_security_group.network-sg01.name
 }
 
 resource "azurerm_network_security_rule" "edb-pgsql" {
